2abc8cce063a6737d9330edde2e96dc4907a8344
Ömer <omergungorco@gmail.com>
2025-10-22 02:21:28 +0300

    gtag code added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +13,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17670181553"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17670181553');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         {children}
       </body>
